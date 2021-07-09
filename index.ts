f198ba7e15ad7824e86cc467bb3d9844546ce3b9
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2021-07-09 16:22:51 -0300

    refactor: Separating the registers from di container

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import app from './infrastructure/server/server';
 import { asClass, InjectionMode, createContainer, asValue } from 'awilix';
 import UserRepository from './infrastructure/repositories/User/UserRepository';
-import { CreateUser } from './application/use_cases/User/';
+import { CreateUser, MergeUser, DeleteUser, GetUserByEmail, GetUserById } from './application/use_cases/User/';
 import UserController from './interfaces/controller/User/UserController';
 import { user } from './infrastructure/orm/sequelize/models';
 import express from 'express';
@@ -10,12 +10,26 @@ const container = createContainer({
   injectionMode: InjectionMode.PROXY
 });
 
+//cases
 container.register({
-  userController: asClass(UserController).singleton(),
-  userRepository: asClass(UserRepository).scoped(),
-  createUser: asClass(CreateUser).scoped(),
-})
+  createUser: asClass(CreateUser).singleton(),
+  mergeUser: asClass(MergeUser).singleton(),
+  deleteUser: asClass(DeleteUser).singleton(),
+  getUserById: asClass(GetUserById).singleton(),
+  getUserByEmail: asClass(GetUserByEmail).singleton(),
+});
 
+//repositories
+container.register({
+  userRepository: asClass(UserRepository).scoped(),
+});
+
+//controllers
+container.register({
+  userController: asClass(UserController).scoped(),
+});
+
+//database
 container.register({
   UserModel: asValue(user),
 });
